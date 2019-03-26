d6b786a90e09a61be73c8f2be64e912f176fcc4e
mrkumar0 <maniranjan0@gmail.com>
2019-03-26 19:54:39 +0530

    adding image and data

--- js/xml.dataTables.script.js
+++ js/xml.dataTables.script.js
@@ -11,12 +11,12 @@ var xhttp;
   xhttp.open("GET","xml/patner.xml",false);
   xhttp.send();
   var xmlDoc = xhttp.responseXML;
-  var tabular = '<table style="width: 100%;" id="example" class="vitamins"><tfoot><tr><th>Image</th><th>Gadi From</th><th>Model</th><th>Number</th><th>From</th><th>To</th><th>Driver No.</th></tr></tfoot><thead><tr><th>image</th><th>gadi from</th><th>model</th><th>number</th><th>from</th><th>to</th><th>Driver No.</th></tr></thead><tbody>';
+  var tabular = '<table style="width: 100%;" id="example" class="vitamins"><tfoot><tr><th>Image</th><th>Model</th><th>Number</th><th>Gadi From</th><th>Driver No.</th></tr></tfoot><thead><tr><th>image</th><th>model</th><th>number</th><th>gadi from</th><th>Driver No.</th></tr></thead><tbody>';
   var i;
   var x = xmlDoc.getElementsByTagName("id");
   for(i=0;i<x.length;i++)
   {
-    tabular += '<tr><td><img src="'+xmlDoc.getElementsByTagName('image')[i].childNodes[0].nodeValue+'" class="img-responsive"></td><td>'+xmlDoc.getElementsByTagName('gadi')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('model')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('number')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('from')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('to')[i].childNodes[0].nodeValue+'</td><td><button type="button" class="btn btn-success" onclick="getConfirmation('+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+')">'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'</button><a id="'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'" href="tel:'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'"></a></td></tr>';
+    tabular += '<tr><td><img src="images/'+xmlDoc.getElementsByTagName('gadi_number')[i].childNodes[0].nodeValue+'.jpg" class="img-responsive"></td><td>'+xmlDoc.getElementsByTagName('gadi_model')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('gadi_number')[i].childNodes[0].nodeValue+'</td><td>'+xmlDoc.getElementsByTagName('gadi_from')[i].childNodes[0].nodeValue+'</td><td><button type="button" class="btn btn-success" onclick="getConfirmation('+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+')">'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'</button><a id="'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'" href="tel:'+xmlDoc.getElementsByTagName('driver_number')[i].childNodes[0].nodeValue+'"></a></td></tr>';
   }
   tabular += '</tbody></table>';
   document.getElementById("slideid").innerHTML = tabular;
